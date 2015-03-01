94c29d1fde8ce5a2452331b6b03cdc4140f734e9
Kasper Tvede <kasper@webmasteren.eu>
2015-03-01 20:10:51 +0300

    handle another funky case ..

--- winforms-collection/SharedFunctionalities/SmartUITimer.cs
+++ winforms-collection/SharedFunctionalities/SmartUITimer.cs
@@ -11,29 +11,31 @@ namespace SharedFunctionalities {
 
         private Control ui;
 
-        public SmartUITimer( Control ui ) {
+        public SmartUITimer(Control ui) {
             this.ui = ui;
         }
 
-        public override void start( Action<object, ElapsedEventArgs, SmartTimer> handler, Action after ) {
-            base.start( ( object sender, ElapsedEventArgs e, SmartTimer timer ) => { handleInvoke( handler, sender, e, timer ); }, () => { handleInvoke( after ); } );
+        public override void start(Action<object , ElapsedEventArgs , SmartTimer> handler , Action after) {
+            base.start((object sender , ElapsedEventArgs e , SmartTimer timer) => { handleInvoke(handler , sender , e , timer); } , () => { handleInvoke(after); });
         }
 
 
-        private void handleInvoke( Action<object, ElapsedEventArgs, SmartTimer> act, object sender, ElapsedEventArgs e, SmartTimer timer ) {
-            if ( ui.InvokeRequired ) {
-                ui.BeginInvoke( (MethodInvoker)(() => { act.Invoke( sender, e, timer ); }) );
-            } else {
-                act.Invoke( sender, e, timer );
+        private void handleInvoke(Action<object , ElapsedEventArgs , SmartTimer> act , object sender , ElapsedEventArgs e , SmartTimer timer) {
+            if (ui.IsHandleCreated && !ui.IsDisposed) {
+                if (ui.InvokeRequired) {
+                    ui.BeginInvoke((MethodInvoker) (() => { act.Invoke(sender , e , timer); }));
+                } else {
+                    act.Invoke(sender , e , timer);
+                }
             }
         }
 
-        private void handleInvoke( Action act ) {
-            if ( act == null || ui.Disposing || ui.IsDisposed || (!ui.IsHandleCreated) ) {
+        private void handleInvoke(Action act) {
+            if (act == null || ui.Disposing || ui.IsDisposed || (!ui.IsHandleCreated)) {
                 return;
             }
-            if ( ui.InvokeRequired ) {
-                ui.BeginInvoke( (MethodInvoker)(() => { act.Invoke(); }) );
+            if (ui.InvokeRequired) {
+                ui.BeginInvoke((MethodInvoker) (() => { act.Invoke(); }));
             } else {
                 act.Invoke();
             }
