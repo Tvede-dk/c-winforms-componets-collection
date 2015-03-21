e01ddf21d298cd850e25d01bcfd965d0e979dbd2
Kasper Tvede <kasper@webmasteren.eu>
2015-03-21 17:36:36 +0100

    changed how to add lists, since the generic would otherwise be chosen.

--- winforms-collection/winforms-collection/lists/SimpleList.cs
+++ winforms-collection/winforms-collection/lists/SimpleList.cs
@@ -8,10 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace winforms_collection
-{
-    public partial class SimpleList : UserControl
-    {
+namespace winforms_collection {
+    public partial class SimpleList : UserControl {
 
         private ListViewItem selectedObject;
 
@@ -21,74 +19,57 @@ namespace winforms_collection
         #region event handlers
 
 
-        public void SetData<T>(IEnumerable<T> dataLst)
-        {
+        public void SetData<T>(IEnumerable<T> dataLst) {
             Clear();
             AddData(dataLst);
         }
 
-        public void AddData<T>(T dataObj)
-        {
+        public void AddData<T>(T dataObj) {
             data.Add(dataObj);
-            if (onRenderCallback != null)
-            {
+            if (onRenderCallback != null) {
                 ListViewItem lvi = onRenderCallback((T)dataObj);
                 simpleListControl1.Items.Add(lvi);
             }
 
         }
 
-        public void AddData<T>(IEnumerable<T> dataLst)
-        {
-            foreach (var item in dataLst)
-            {
+        public void AddDataCollection<T>(IEnumerable<T> dataLst) {
+            foreach (var item in dataLst) {
                 AddData(item);
             }
         }
 
 
-        public void removeAt(int index)
-        {
+        public void removeAt(int index) {
             simpleListControl1.Items.RemoveAt(index);
             data.RemoveAt(index);
         }
 
-        public void callEdit<T>(T obj, int v)
-        {
+        public void callEdit<T>(T obj, int v) {
             callEdit((object)obj, v);
         }
-        private void callEdit(object obj, int v)
-        {
-            if (onEditCallback != null)
-            {
+        private void callEdit(object obj, int v) {
+            if (onEditCallback != null) {
                 onEditCallback(obj, v);
             }
         }
 
 
-        public void editObject(object newObj, int index)
-        {
+        public void editObject(object newObj, int index) {
             bool addedInstead = false;
-            if (data.Count == 0 || index < 0 || index > data.Count)
-            {
+            if (data.Count == 0 || index < 0 || index > data.Count) {
                 data.Add(newObj);
                 addedInstead = true;
                 index = data.Count - 1;
-            }
-            else
-            {
+            } else {
                 data[index] = newObj;
             }
 
-            if (onRenderCallback != null)
-            {
+            if (onRenderCallback != null && newObj != null) {
                 ListViewItem lvi = onRenderCallback(newObj);
-                if (addedInstead)
-                {
+                if (addedInstead) {
                     simpleListControl1.Items.Insert(index, lvi);
-                }
-                else
-                {
+                } else {
                     simpleListControl1.Items[index] = lvi;
                 }
 
@@ -96,30 +77,26 @@ namespace winforms_collection
 
         }
 
-        public void setHandlers<T>(Action onAddHandler, Action<T, int> onEditHandler, Action<T, int> onRemoveHandler, Func<T, ListViewItem> renderFunction)
-        {
+        public void setHandlers<T>(Action onAddHandler, Action<T, int> onEditHandler, Action<T, int> onRemoveHandler, Func<T, ListViewItem> renderFunction) {
             onAdd(onAddHandler);
             onEdit(onEditHandler);
             onRemove(onRemoveHandler);
             onRender(renderFunction);
         }
 
-        private void onAdd(Action onAddHandler)
-        {
+        private void onAdd(Action onAddHandler) {
             onAddCallback = onAddHandler;
         }
 
         private Action onAddCallback = null;
 
-        public void onEdit<T>(Action<T, int> onEditHandler)
-        {
+        public void onEdit<T>(Action<T, int> onEditHandler) {
             onEditCallback = ((object parm, int val) => { onEditHandler((T)parm, val); });
         }
 
         private Action<object, int> onEditCallback = null;
 
-        public void onRemove<T>(Action<T, int> onRemoveHandler)
-        {
+        public void onRemove<T>(Action<T, int> onRemoveHandler) {
             this.onRemoveHandler = ((object obj, int val) => { onRemoveHandler((T)obj, val); });
         }
 
@@ -128,19 +105,16 @@ namespace winforms_collection
         #endregion
 
 
-        public void onRender<T>(Func<T, ListViewItem> onRender)
-        {
+        public void onRender<T>(Func<T, ListViewItem> onRender) {
             onRenderCallback = new Func<object, ListViewItem>((object obj) => { return onRender((T)obj); });//wrap it around.
         }
 
-        private Func<object, ListViewItem> onRenderCallback = (object obj) =>
-        {
+        private Func<object, ListViewItem> onRenderCallback = (object obj) => {
             return new ListViewItem(obj.ToString());
         };
 
 
-        protected override void OnResize(EventArgs e)
-        {
+        protected override void OnResize(EventArgs e) {
             base.OnResize(e);
             if (Width < 280) { //approx 
                 button1.Text = "";
@@ -163,19 +137,16 @@ namespace winforms_collection
         private String _title;
 
 
-        public String title
-        {
+        public String title {
             get { return _title; }
-            set
-            {
+            set {
                 _title = value;
                 label1.Text = value;
             }
         }
         #endregion
 
-        public SimpleList()
-        {
+        public SimpleList() {
             onAdd(() => { callEdit(null, -1); });
             onRemove((object obj, int index) => { removeAt(index); });
             InitializeComponent();
@@ -184,8 +155,7 @@ namespace winforms_collection
         /// <summary>
         /// Clears the data in this list. 
         /// </summary>
-        public void Clear()
-        {
+        public void Clear() {
             data.Clear();
             selectedObject = null;
             simpleListControl1.Clear();
@@ -194,45 +164,34 @@ namespace winforms_collection
         [EditorBrowsable]
         [DefaultValue(View.List)]
         [Description("The way the listview gets rendered")]
-        public View View
-        {
-            get
-            {
+        public View View {
+            get {
                 return simpleListControl1.View;
             }
-            set
-            {
+            set {
                 simpleListControl1.View = value;
             }
         }
 
         #region events / click handlers internal 
-        private void simpleListControl1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (simpleListControl1.SelectedItems.Count == 1)
-            {
+        private void simpleListControl1_SelectedIndexChanged(object sender, EventArgs e) {
+            if (simpleListControl1.SelectedItems.Count == 1) {
                 selectedObject = simpleListControl1.SelectedItems[0];
-            }
-            else
-            {
+            } else {
                 selectedObject = null;
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (onRemoveHandler != null && selectedObject != null)
-            {
+        private void button3_Click(object sender, EventArgs e) {
+            if (onRemoveHandler != null && selectedObject != null) {
                 int index = simpleListControl1.SelectedIndices[0];
                 onRemoveHandler(data[index], index);
 
             }
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (onEditCallback != null && selectedObject != null)
-            {
+        private void button4_Click(object sender, EventArgs e) {
+            if (onEditCallback != null && selectedObject != null) {
                 int index = simpleListControl1.SelectedIndices[0];
                 onEditCallback(data[index], index);
             }
@@ -240,10 +199,8 @@ namespace winforms_collection
 
 
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (onAddCallback != null)
-            {
+        private void button1_Click(object sender, EventArgs e) {
+            if (onAddCallback != null) {
                 onAddCallback();
             }
         }
