fed3a83d6b04129b692147d1457c866f6ef2c537
Kasper Tvede <kasper@webmasteren.eu>
2015-06-29 02:57:40 +0200

    more bug cathing (since it is so unreliable). and started on a simple wrapper for file handling to provide Memory mapped files.

--- winforms-collection/winforms-collection/popup boxes/FastNoticePopup.cs
+++ winforms-collection/winforms-collection/popup boxes/FastNoticePopup.cs
@@ -10,45 +10,49 @@ namespace winforms_collection {
     public partial class FastNoticePopup : Form {
         private int displayTime;
 
-        public FastNoticePopup( String text, int displayTime, Bitmap bmp, Form parrent = null ) {
+        public FastNoticePopup(String text, int displayTime, Bitmap bmp, Form parrent = null) {
             InitializeComponent();
-            if ( parrent == null ) {
+            if (parrent == null) {
                 parrent = this;
             }
             this.StartPosition = FormStartPosition.Manual;
-            handleLocation( parrent );
+            handleLocation(parrent);
             this.label1.Text = text;
             this.pictureBox1.Image = bmp;
             this.displayTime = displayTime;
             this.Opacity = 0f;
         }
-        protected override void OnLoad( EventArgs e ) {
-            base.OnLoad( e );
-            this.FadeIn( displayTime / 4, afterFadeIn );
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+            this.FadeIn(displayTime / 4, afterFadeIn);
         }
 
-        private void handleLocation( Form parrent ) {
-            parrent.handleRemoteInvoke( () => {
-                Screen parrentScreen = Screen.FromControl( parrent );
-                this.handleRemoteInvoke( () => {
+        private void handleLocation(Form parrent) {
+            parrent.handleRemoteInvoke(() => {
+                Screen parrentScreen = Screen.FromControl(parrent);
+                this.handleRemoteInvoke(() => {
 
                     Rectangle screen = parrentScreen.Bounds;
                     Rectangle work = parrentScreen.WorkingArea;
-                    this.Top = (work.Bottom - (this.Height * 2) - 25) - 50;
-                    this.Left = work.Right - (screen.Width / 2) - (this.Width / 2);
-                    int nTaskBarHeight = screen.Bottom - parrentScreen.WorkingArea.Bottom;
-                } );
-            } );
+                    try {
+                        this.Top = (work.Bottom - (this.Height * 2) - 25) - 50;
+                        this.Left = work.Right - (screen.Width / 2) - (this.Width / 2);
+                        int nTaskBarHeight = screen.Bottom - parrentScreen.WorkingArea.Bottom;
+                    } catch (Exception e) {
+
+                    }
+                });
+            });
         }
 
 
 
         public void afterFadeIn() {
-            new SmartTimer { repeate = false, counter = 1, interval = displayTime }.start( afterWait ); //wait some time.
+            new SmartTimer { repeate = false, counter = 1, interval = displayTime }.start(afterWait); //wait some time.
         }
 
         public void afterWait() {
-            this.FadeOut( displayTime / 4, afterFadeOut );
+            this.FadeOut(displayTime / 4, afterFadeOut);
         }
 
         public void afterFadeOut() {
@@ -56,23 +60,23 @@ namespace winforms_collection {
             Dispose();
         }
 
-        public static void Show( string text, Bitmap bmp ) {
+        public static void Show(string text, Bitmap bmp) {
         }
 
 
-        public static void ShowInfo( string text, Form parret ) {
+        public static void ShowInfo(string text, Form parret) {
             //IN ORder to use the showtopmostinactivce, we would have to schedual the timers on the parent form..  SO this is a "WIP" / TODO.
-            new FastNoticePopup( text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416174868_info, parret ).Show();
+            new FastNoticePopup(text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416174868_info, parret).Show();
         }
 
-        public static void ShowSucess( string text, Form parret ) {
-            new FastNoticePopup( text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416175596_ok, parret ).Show();
+        public static void ShowSucess(string text, Form parret) {
+            new FastNoticePopup(text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416175596_ok, parret).Show();
         }
-        public static void ShowWarning( string text, Form parret ) {
-            new FastNoticePopup( text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416175496_alert, parret ).Show();
+        public static void ShowWarning(string text, Form parret) {
+            new FastNoticePopup(text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416175496_alert, parret).Show();
         }
-        public static void ShowError( string text, Form parret ) {
-            new FastNoticePopup( text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416175115_delete, parret ).Show();
+        public static void ShowError(string text, Form parret) {
+            new FastNoticePopup(text, 1200 + (text.Length / 6 * 100), Properties.Resources._1416175115_delete, parret).Show();
         }
     }
 }
