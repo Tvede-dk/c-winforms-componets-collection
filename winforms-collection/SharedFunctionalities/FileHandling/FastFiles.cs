b6ff0dc1aa63e69fcd176b5e4c9cec1adceff634
Kasper Tvede <kasper@webmasteren.eu>
2015-06-29 03:06:49 +0200

    Revert "..."
    This reverts commit febdee5d546b7fe6df777c4b6d8d9a16fbeff81e.

---
+++ winforms-collection/SharedFunctionalities/FileHandling/FastFiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedFunctionalities.FileHandling {
+    /// <summary>
+    /// Idea is to use memory mapped files for operations to maximize performance. and introduce more async code execution.
+    /// </summary>
+    public class FastFiles {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dest"></param>
+        /// <param name="overrideIfExists"></param>
+        public async static void moveFileAsync(string src, string dest, bool overrideIfExists) {
+            if ((overrideIfExists || !File.Exists(dest)) && File.Exists(src)) {
+                var destFile = MemoryMappedFile.CreateFromFile(src, FileMode.Create);
+                var srcFile = MemoryMappedFile.CreateFromFile(src, FileMode.Open);
+                if (destFile != null && srcFile != null) { //make sure that every file is accessable
+                    using (var destBuff = destFile.CreateViewStream()) { // and make sure that c# handles the handles.
+                        using (var srcBuff = srcFile.CreateViewStream()) { //this also works for exceptions. ;) 
+                            await srcBuff.CopyToAsync(destBuff);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
