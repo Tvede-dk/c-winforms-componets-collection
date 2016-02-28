90a21bc6fa4d73d350bf78df611d00da94864fda
Kasper Tvede <kasper@webmasteren.eu>
2016-02-28 22:35:49 +0100

    added more helpers.

--- winforms-collection/SharedFunctionalities/extensions/FormExtensions.cs
+++ winforms-collection/SharedFunctionalities/extensions/FormExtensions.cs
@@ -29,40 +29,61 @@ public static class FormExtensions {
          int Y,                // Vertical position
          int cx,               // Width
          int cy,               // Height
-         uint uFlags );         // Window positioning flags
+         uint uFlags);         // Window positioning flags
 
     [DllImport("user32.dll")]
-    static extern bool ShowWindow( IntPtr hWnd, int nCmdShow );
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-    public static void ShowInactiveTopmost<T>( this T frm ) where T : Form {
-        ShowWindow( frm.Handle, SW_SHOWNOACTIVATE );
-        SetWindowPos( frm.Handle, HWND_TOPMOST,
+    public static void ShowInactiveTopmost<T>(this T frm) where T : Form {
+        ShowWindow(frm.Handle, SW_SHOWNOACTIVATE);
+        SetWindowPos(frm.Handle, HWND_TOPMOST,
         frm.Left, frm.Top, frm.Width, frm.Height,
-        SWP_NOACTIVATE );
+        SWP_NOACTIVATE);
     }
     #endregion
 
-    public static void setTopMost<T>(this T f) where T: Form {
+    public static void setTopMost<T>(this T f) where T : Form {
         SetWindowPos(f.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
 
     }
 
     #region animations
 
-    public static void FadeIn<T>( this T frm, int displayTimeInMs, Action after, float maxVal = 1f, float startVal = 0f ) where T : Form {
-        SharedAnimations.fadeIn( frm, displayTimeInMs, after, maxVal, startVal );
+    public static void FadeIn<T>(this T frm, int displayTimeInMs, Action after, float maxVal = 1f, float startVal = 0f) where T : Form {
+        SharedAnimations.fadeIn(frm, displayTimeInMs, after, maxVal, startVal);
     }
-    public static void FadeOut<T>( this T frm, int displayTimeInMs, Action after, float minVal = 1f, float startVal = 0f ) where T : Form {
-        SharedAnimations.fadeOut( frm, displayTimeInMs, after, minVal, startVal );
+    public static void FadeOut<T>(this T frm, int displayTimeInMs, Action after, float minVal = 1f, float startVal = 0f) where T : Form {
+        SharedAnimations.fadeOut(frm, displayTimeInMs, after, minVal, startVal);
     }
 
-    public static void handleRemoteInvoke(this Control con, Action code ) {
-        if ( con.InvokeRequired ) {
-            con.BeginInvoke( code );
+    public static void handleRemoteInvoke(this Control con, Action code) {
+        if (con.InvokeRequired) {
+            con.BeginInvoke(code);
         } else {
             code.Invoke();
         }
     }
+
+    #endregion
+
+
+    #region Dialogboxes
+
+
+    public static void PerformIfOkay(this DialogResult result, Action onOk, DialogResult okCondition = DialogResult.OK) {
+        if (result == okCondition) {
+            onOk();
+        }
+    }
+
+    public static void DoInvokeIfRequired(this Control control, Action callback) {
+        if (control.InvokeRequired) {
+            control.BeginInvoke(new MethodInvoker(callback));
+        } else {
+            callback();
+        }
+    }
+
 
     #endregion
 
