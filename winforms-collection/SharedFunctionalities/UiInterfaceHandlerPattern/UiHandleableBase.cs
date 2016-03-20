e13e8d2a86d4a29049518e33c76f6f7d8620b714
Kasper Tvede <kasper@webmasteren.eu>
2016-03-20 12:33:05 +0100

    Added a simple pair of interfaces and a handler class, meant for dealing with coupling different uis to objects.

---
+++ winforms-collection/SharedFunctionalities/UiInterfaceHandlerPattern/UiHandleableBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedFunctionalities.UiInterfaceHandlerPattern {
+    /// <summary>
+    /// intention: to be able to pair a ui with an object of a required / given type.
+    /// </summary>
+    public interface UiHandleableInterface {
+        /// <summary>
+        /// gets the type name of the matching object.
+        /// </summary>
+        /// <returns>the same name as the UIObjectHandleableInterface would return.</returns>
+        string getMatchingTypeName();
+    }
+
+    /// <summary>
+    /// intention: to be able to pair this object with a given UI. eg the UIHandleableInterface
+    /// </summary>
+    public interface UiObjectHandleableInterface {
+        /// <summary>
+        /// Gets the "typename" for this (to be paired with a ui).
+        /// </summary>
+        /// <returns>the name of the type of this object. can be the default typesystem's name, or a hardcoded provided one. </returns>
+        string getMatchingTypeName();
+    }
+}
---
+++ winforms-collection/SharedFunctionalities/UiInterfaceHandlerPattern/UiToObjectHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedFunctionalities.UiInterfaceHandlerPattern {
+    public class UiToObjectHandler<T> where T : UiObjectHandleableInterface {
+        private Dictionary<string, Func<T, UiHandleableInterface>> typeToUi = new Dictionary<string, Func<T, UiHandleableInterface>>();
+
+        public UiToObjectHandler<T> AddUiForType(string typename, Func<T, UiHandleableInterface> constructorFunction) {
+            typeToUi.Add(typename, constructorFunction);
+            return this;
+        }
+
+        /// <summary>
+        /// creates a ui from an
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>null if unable to pair.</returns>
+        public UiHandleableInterface createUiForObject(T obj) {
+            return typeToUi.ContainsKey(obj.getMatchingTypeName()) ? typeToUi[obj.getMatchingTypeName()](obj) : null;
+        } 
+
+
+    }
+}
